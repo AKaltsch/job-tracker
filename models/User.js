47685e51e2510b28fee1abd8a80bf65e83b2b7b9
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-11-07 21:06:52 -0800

    jwt token created

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -22,9 +24,21 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, "Please enter password"],
     minlength: 6,
+    select: false,
   },
   lastName: { type: String, trim: true, maxlength: 20, default: "lastName" },
   location: { type: String, trim: true, maxlength: 20, default: "My City" },
 });
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 export default mongoose.model("User", UserSchema);
