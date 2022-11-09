214c54f0047b0b7eb0ac65d2606402bbab1a083a
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-11-09 12:11:00 -0800

    Nested routes set up for dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Error, Landing, Register, Dashboard } from "./pages";
+import { Error, Landing, Register, ProtectedRoute } from "./pages";
+import {
+  AllJobs,
+  AddJob,
+  Profile,
+  SharedLayout,
+  Stats,
+} from "./pages/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />}></Route>
+          <Route path="add-job" element={<AddJob />}></Route>
+          <Route path="profile" element={<Profile />}></Route>
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
