2c65c83ebe78ae0e4e3fe16be655294561b1b123
Alec Kaltschmid <kaltschmidalec@gmail.com>
2022-11-09 20:47:56 -0800

    both sidebars and navbar complete

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,26 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppContext } from "../context/appContext";
+import Logo from "../components/Logo";
+import NavLinks from "./NavLinks";
 
 const BigSidebar = () => {
+  // import toggleSidebar and pass it into NavLinks to close sidebar on page selection
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h1>BigSidebar</h1>
+      <div
+        className={
+          showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
